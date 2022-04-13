7949bbb1d8fbae824bcf613f9c2f3b4de83d26c0
110213041 <s110213041@mail1.ncnu.edu.tw>
2022-04-13 16:42:26 +0800

    update alert cookies message

--- scripts.js
+++ scripts.js
@@ -1,9 +1,11 @@
+// disable right click menu
 window.oncontextmenu = (e) => {
     e.preventDefault();
 }
 
 const catClass = document.getElementById("cat-btn").classList;
 
+// create popCount for new visitor
 if (!localStorage.popCount) {
     localStorage.popCount = 0;
 }
@@ -12,18 +14,29 @@ function updatePopCount() {
     document.getElementById("pop-counter").innerHTML = localStorage.popCount;
 }
 
-updatePopCount()
+// No cookies alert box
+if (!sessionStorage.isViewed || !Boolean(sessionStorage.isViewed)) {
+    alert("Our website don't use cookies!");
+    sessionStorage.isViewed = true;
+    // update the beginning number, 0 if new visitor.
+    updatePopCount()
+} else {
+    updatePopCount()
+}
 
+// click the cat, open her mouth.
 const popTheCat = () => {
     catClass.replace("cat-close", "cat-open");
     localStorage.popCount = Number(localStorage.popCount) + 1;
     updatePopCount()
 }
 
+// shut her mouth
 const closeTheCat = () => {
     catClass.replace("cat-open", "cat-close");
 }
 
+// reset the counter
 const resetPopCount = () => {
     localStorage.popCount = 0;
     updatePopCount()
